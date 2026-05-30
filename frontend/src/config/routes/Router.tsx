a52da57ac006baf5b4ba452744c86d0671326117
Nilton Guerrero <niltonguerrero542@gmail.com>
2026-05-30 17:43:34 -0500

    Implementacion de la pagina cargando

--- frontend/src/config/routes/Router.tsx
+++ frontend/src/config/routes/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Inicio from "../../modules/home/pages/Inicio";
+import Cargando from "../../modules/home/pages/cargando";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Inicio />} />
+        <Route path="/cargando" element={<Cargando />} />
       </Routes>
     </BrowserRouter>
   );
