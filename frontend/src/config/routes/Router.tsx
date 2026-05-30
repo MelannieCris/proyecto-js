b084191990b6aa3d5c0e7bb37ec21964636cd396
Nilton Guerrero <niltonguerrero542@gmail.com>
2026-05-30 17:49:56 -0500

    Implementacion de la pagina NO ENCONTRADA

--- frontend/src/config/routes/Router.tsx
+++ frontend/src/config/routes/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Inicio from "../../modules/home/pages/Inicio";
 import Cargando from "../../modules/home/pages/cargando";
+import PaginaNoEncontrada from "../../modules/home/pages/PaginaNoEncontrada";
 
 export default function Router() {
   return (
@@ -8,6 +9,7 @@ export default function Router() {
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/cargando" element={<Cargando />} />
+        <Route path="*" element={<PaginaNoEncontrada />} />
       </Routes>
     </BrowserRouter>
   );
